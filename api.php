5f3a98a182db52960c9865caace1c88c5798814f
Tom Dale <tom@zero.co.nz>
2013-04-18 05:00:38 +0000

    Video - Change filter to output rather than content

--- api.php
+++ api.php
@@ -3,7 +3,7 @@
 Jojo::addHook('admin_action_after_save_video', 'admin_action_after_save_video', 'jojo_video');
 Jojo::addHook('foot', 'js', 'jojo_video');
 
-Jojo::addFilter('content', 'content', 'jojo_video');
+Jojo::addFilter('output', 'content', 'jojo_video');
 
 $_provides['fieldTypes'] = array(
         'videoembedcode'         => 'Video embed code'
